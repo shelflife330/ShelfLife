9fad8f636ab71c764c8828f809c2e722cf8f17a9
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2019-05-27 15:57:40 -0500

    Json dynamically renders

--- scripts/search.js
+++ scripts/search.js
@@ -1,65 +1,100 @@
-var textbooks = [{
-  title: "Currents",
-  author: "Tame Impala",
-  class: ["EA1", "Math 240"],
-  condition: "poor",
-  price: 20.99,
-  isbn13: "978-0321982384",
-}, {
-  title: "Currents",
-  author: "Tame Impala",
-  class: ["EA1", "Math 240"],
-  condition: "poor",
-  price: 20.99,
-  isbn13: "978-0321982384",
-}, {
-  title: "Currents",
-  author: "Tame Impala",
-  class: ["EA1", "Math 240"],
-  condition: "poor",
-  price: 20.99,
-  isbn13: "978-0321982384",
-}, {
-  title: "Currents",
-  author: "Tame Impala",
-  class: ["EA1", "Math 240"],
-  condition: "poor",
-  price: 20.99,
-  isbn13: "978-0321982384",
-}];
+var textbooks = [
+  {
+    title: "Linear Algebra and its Applications",
+    author: "David C. Lay",
+    class: [
+      "EA1", "Math 240"
+    ],
+    condition: "Used-Good",
+    price: 45,
+    isbn13: "978-0321982384"
+  }, {
+    title: "Short Guide to Writing About Art",
+    author: "Sylvan Barnet",
+    class: [
+      "AH 101"
+    ],
+    condition: "Used-Good",
+    price: 15,
+    isbn13: "978-0205886999"
+  }, {
+    title: "Introductory Econometrics: A Modern Approach",
+    author: "Jeffery Wooldridge",
+    class: [
+      "Math 386"
+    ],
+    condition: "New",
+    price: 15,
+    isbn13: "978-1111531041"
+  }, {
+    title: "Minds on trial: Great Cases in Law and Psychology",
+    author: "Charles Patrick Ewing",
+    class: [
+      "Psych 340"
+    ],
+    condition: "Poor",
+    price: 8,
+    isbn13: "978-0195181760"
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: "Randal Bryant",
+    class: [
+      "EECS 213"
+    ],
+    condition: "Used-Good",
+    price: 70,
+    isbn13: "978-013409266"
+  },
+  {
+    title: "Sociology: A Down-To-Earth Approach",
+    author: "James M. Henslin",
+    class: [
+      "Soc 101"
+    ],
+    condition: "Poor",
+    price: 40,
+    isbn13: "978-0205096541"
+  }
+];
 
 function buildTable() {
-  var albumList = document.getElementById("table-list");
-}
+  $.getJSON("json_files/textbooks.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+  });
+  var textbookList = document.getElementById("myTable");
+  if (textbookList != null) {
+    for (var i = 0; i < textbooks.length; i++) {
+      var textbook = document.createElement("tr");
+      let classes = "";
 
-function buildAlbumList() {
-  var albumList = document.getElementById("album-list"); //get the albumList which is the parent for what we are creating
-  if (albumList != null) {
-    for (var i = 0; i < albums.length; i++) {
-      if (i % 2 == 0) { //when i is even we put the album on the left
-        var rowDiv = document.createElement('div'); //we only create a row once per 2 albums
-        rowDiv.classList.add('row_wrapper');
-        albumList.appendChild(rowDiv); //append rowDiv to albumList
-        var albumDiv = document.createElement('div');
-        albumDiv.classList.add('albums-album-wrapper');
-        albumDiv.classList.add('left');
-      } else { //when i is even we put the album on the right
-        var albumDiv = document.createElement('div');
-        albumDiv.classList.add('albums-album-wrapper');
-        albumDiv.classList.add('right');
+      //Format for relevant classes
+      for (let j = 0; j < textbooks[i].class.length; j++) {
+        if ((j+1) != textbooks[i].class.length) {
+          classes = classes + textbooks[i].class[j] + ", "
+        }
+        else {
+          classes = classes + textbooks[i].class[j]
+        }
       }
-      rowDiv.appendChild(albumDiv); //append albumDiv to rowDiv
-      var album = document.createElement("div");
+
       var contents = `
-        <img src='${albums[i].cover}' class="albums-album-art">
-        <p class="albums-album-name">${albums[i].title}</p>
-        <p class="albums-artist-name">${albums[i].artist}</p>
-      `; // The contents contain the album cover, name, and artist with manually inserted classes for each
-      album.innerHTML = contents; //make the info of the album object which we created above the contents
-      albumDiv.appendChild(album); //append album to albumDiv
+         <tr>
+        <td>${textbooks[i].title}</td>
+        <td>${textbooks[i].author}</td>
+        <td>${classes}</td>
+        <td>${textbooks[i].condition}</td>
+        <td>${textbooks[i].price}</td>
+        <td>${textbooks[i].isbn13}</td>
+      </tr>
+      `;
+      textbook.innerHTML = contents;
+      textbookList.appendChild(textbook);
     }
   }
 }
+buildTable();
+
 
 $(document).ready(function() {
   $("#myInput").on("keyup", function() {
