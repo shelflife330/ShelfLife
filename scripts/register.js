5d316c3ccd1d0c0451a7411bba653d0d6dcda368
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2019-06-06 00:54:53 -0500

    Added register page

---
+++ scripts/register.js
@@ -0,0 +1,15 @@
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm-password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+//https://codepen.io/diegoleme/pen/surIK
